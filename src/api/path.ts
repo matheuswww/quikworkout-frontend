e7bae78ec0c003f07756b506e0d326da64087956
matheuswww <teteus.30.08.07@outlook.com>
2024-09-30 19:14:57 -0300

    env updated

--- src/api/path.ts
+++ src/api/path.ts
@@ -1,3 +1,3 @@
-export const apiImg = process.env.API_IMG;
-export const api = process.env.API;
-export const env = process.env.ENV;
+export const apiImg = process.env.API_IMG ? process.env.API_IMG : "";
+export const api = process.env.API ? process.env.API : "";
+export const env = process.env.ENV ? process.env.ENV : "";
