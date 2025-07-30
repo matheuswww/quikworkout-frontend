853147a2725925ba687c3302f55f2fb70c74db99
matheuswww <teteus.30.08.07@outlook.com>
2025-07-30 18:20:26 -0300

    deleteCookie updated

--- src/action/deleteCookie.ts
+++ src/action/deleteCookie.ts
@@ -3,6 +3,9 @@
 import { cookies } from 'next/headers';
 
 export async function deleteCookie(key: string) {
+  if (key == "userProfile") {
+    deleteCookie("userGames")
+  }
   cookies().delete({
     name: key,
     path: '/',
